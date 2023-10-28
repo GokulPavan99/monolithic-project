913985a67aab045639946238f47ec1b2c3819b41
root <root@ip-172-31-3-92.ap-south-1.compute.internal>
2023-10-28 07:45:23 +0000

    terraform code is ready

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAZJPQVG4AB3GDAYMU"
+secret_key = "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"
+}
+
+resource "aws_instance" "key" {
+ami = "ami-06006e8b065b5bd46"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
+tags = {
+Name = "production_server"
+}
+}
+
+
+
+
+
+
+
